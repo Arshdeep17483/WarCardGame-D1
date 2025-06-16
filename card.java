86fe3c086711319d7a16f5827564a24442df5187
Arshdeep17483 <sin17483@sheridancollege.ca>
2025-06-16 10:35:40 -0400

    Create card.java

---
+++ card.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermreviewcodeforpartc;
+
+import java.util.Scanner;
+
+/**
+ * The signIn page for UnoOnline.
+ * Takes in a user name and password and then
+ * validates the password before creating a new user in the list
+ * of possible users. To be used as the beginning code for MidtermReview C.
+ * @author dancye, 2019
+ */
+public class UnoOnline 
+{
+    private User users[] = new User[100];//room for 100 online players!
+     
+    /**
+     * Main method with call to private run method, to encapsulate our
+     * main functionality.
+     * @param args - not used
+     */
+    public static void main(String[] args) 
+     {
+       UnoOnline newPortal = new UnoOnline();
+       newPortal.run();
+    }
+     /**
+     * method that takes in the User's name and chosen password
+     * and then continues prompting until the password is valid based on the
+     * following two rules:
+     * 1. The password must be at least length 8
+     * 2. The password must contain at least one "special character"
+     * 
+     */
+    private void run()
+{
+    int userCount = 0; // Track number of users
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("please enter your desired user name:");
+    String userName = sc.nextLine();
+
+    PasswordValidator validator = new PasswordValidator(); // new validator class
+    String password = "";
+    boolean validPassword = false;
+
+    while (!validPassword) {
+        System.out.println("Passwords must have at least 8 characters");
+        System.out.println("Passwords must have at least one special character");
+        System.out.println("Please enter your desired password:");
+
+        password = sc.nextLine();
+        validPassword = validator.isValid(password); // using the class you created
+
+        if (!validPassword) {
+            System.out.println("Password does not meet the requirements. Try again.");
+        }
+    }
+
+    // Create the user after successful validation
+    User newUser = new User(userName, password);
+    users[userCount] = newUser;
+    userCount++;
+
+    System.out.println("New User Added");
+    System.out.println("UserName: " + userName);
+    System.out.println("Password: just kidding, can't show password");
+}
+
+   
+    
+}//end class
